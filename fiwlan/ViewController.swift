8a171e95a4abc0788ec889ed6b370c09b1a06227
Felix <felix.herron@gmail.com>
2017-03-04 10:30:51 +0100

    Initial Commit

---
+++ fiwlan/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  fiwlan
+//
+//  Created by felix on 04/03/17.
+//  Copyright © 2017 fiwlan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
